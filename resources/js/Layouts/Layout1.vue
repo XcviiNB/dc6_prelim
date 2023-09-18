21af659d5c6cdb6897fd59261a84dbc3024cc915
XcviiNB <nathanielbatausafgo@gmail.com>
2023-09-18 09:04:17 +0800

    (not working)

--- resources/js/Layouts/Layout1.vue
+++ resources/js/Layouts/Layout1.vue
@@ -1,9 +1,12 @@
 <script setup>
     import MainNav from '@/Components/MainNav.vue'
+    import DropdownOption from '@/Components/DropdownOption.vue';
     import { ref } from 'vue';
 
     let width = ref('w-[250px]')
     let hide = ref(false)
+    let motif = ref('')
+    let sidebarClr = ref('bg-gray-900')
 
     function toggleWidth() {
         if(width.value == 'w-[250px]') {
@@ -14,22 +17,36 @@
             hide.value = false
         }
     }
+
+    function toggleMotif() {
+        if (motif.value = 'red') {
+            sidebarClr = 'bg-red-500'
+        } else if (motif.value = 'blue') {
+            sidebarClr = 'bg-blue-500'
+        } else {
+            sidebarClr = 'bg-green-500'
+        }
+    }
 </script>
 
 <template>
     <div>
         <!-- <nav class="flex items-center justify-between p-6 bg-gray-800"></nav> -->
         <div class="flex min-h-screen">
-            <div id="sidebar" class="bg-gray-900 p-6 duration-500" :class="width" style="position: relative;">
-                <button class="text-xl text-gray-600" @click="toggleWidth" style="position: absolute; right: 10px; top:10px">
-                    <i class="fa-solid fa-bars text-gray-500"></i>
-                </button>
-                <div id="branding" :hidden="hide" class="my-6">
-                    <img src="assets/N.png" alt="Logo"
-                        class="w-[170px] h-[170px] mx-auto rounded-full object-cover">
+            <div :class="sidebarClr">
+                <div id="sidebar" class="p-6 duration-500" :class="width" style="position: relative;">
+                    <button class="text-xl text-gray-600" @click="toggleWidth" style="position: absolute; right: 10px; top:10px">
+                        <i class="fa-solid fa-bars text-gray-500"></i>
+                    </button>
+                    <div id="branding" :hidden="hide" class="my-6">
+                        <img src="assets/N.png" alt="Logo"
+                            class="w-[170px] h-[170px] mx-auto rounded-full object-cover">
+                    </div>
+                    <hr class="border-gray-600" :hidden="hide">
+                    <MainNav :hidden="hide"></MainNav>
+
+                    <DropdownOption />
                 </div>
-                <hr class="border-gray-600" :hidden="hide">
-                <MainNav :hidden="hide"></MainNav>
             </div>
 
             <div id="container" class="flex-1">
